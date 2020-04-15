a0df589c6ea54466fcb2705a5dcccf109dab01a7
Sriharish Koka <skoka@netflix.com>
2020-04-14 17:20:13 -0700

    Fixing lightcycle keymap to meet the layout for the base layer

--- keyboards/handwired/dactyl_promicro/keymaps/lightcycle/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/lightcycle/keymap.c
@@ -40,20 +40,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_lightcycle(  // layer 0 : default
         // left hand
-          KC_ESC,       KC_Q,      KC_W,     KC_E,     KC_R,    KC_T,
-          KC_TAB,       KC_A,      KC_S,     KC_D,     KC_F,    KC_G,
-          KC_LBRC,      KC_Z,      KC_X,     KC_C,     KC_V,    KC_B,
-          KC_LCTL,      KC_LALT,   KC_LALT,  KC_LEFT,  KC_RGHT,
-                                                                          KC_LCTL, KC_RGUI,
-                                                                KC_BSPC,  KC_LSFT, KC_LGUI,
-                                                                                          
-        // right hand                                                                     
-                            KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,        KC_EQL,
-                            KC_H,   KC_J,     KC_K,     KC_L,     KC_SCLN,     KC_QUOT,
-                            KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,     KC_RBRC,
-                                    KC_UP,    KC_DOWN,  KC_MINS,  KC_BSLS,     KC_GRAVE,
-        MO(SYMB),  KC_DEL,
-        KC_ENT,    MO(NUMB), KC_SPC 
+        KC_ESC,       KC_Q,      KC_W,     KC_E,     KC_R,    KC_T,                                               KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_EQL,
+        KC_TAB,       KC_A,      KC_S,     KC_D,     KC_F,    KC_G,                                               KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,
+        KC_LBRC,      KC_Z,      KC_X,     KC_C,     KC_V,    KC_B,                                               KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RBRC,
+        KC_LCTL,      KC_LALT,   KC_LALT,  KC_LEFT,  KC_RGHT,                                                            KC_UP,  KC_DOWN,  KC_MINS,  KC_BSLS,  KC_GRAVE,
+                                                                        KC_LCTL, KC_RGUI,     MO(SYMB), KC_DEL,
+                                                              KC_BSPC,  KC_LSFT, KC_LGUI,     KC_ENT,   MO(NUMB), KC_SPC 
     ),
 
 /* Keymap 1: Number Layer
